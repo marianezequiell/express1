89f26c95f6e295d4c7857b7dc0d609c719bc4527
marianoEzequiell <mariano_ef@live.com>
2021-11-28 15:30:06 -0300

    Corregido

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const Contenedor = require('./Contenedor.js')
 
 const app = express()
 
-let seeProducts = new Contenedor()
+let seeProducts = new Contenedor('productos')
+let data
+(async function getData () {
+    data = await seeProducts.getAll()
+})()
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -11,8 +15,8 @@ function getRandomInt(min, max) {
 
 const server = app.listen(process.env.PORT || 3000)
 
-app.get('/', (request, response) => {
-    response.send(
+app.get('/', (req, res) => {
+    res.send(
         `
             <ul>
                 <li><a href="/productos">Listado de productos</a></li>
@@ -22,11 +26,25 @@ app.get('/', (request, response) => {
     )
 })
 
-app.get('/productos', (request, response) => {
-    response.send(`${seeProducts.getAll()}`)
+app.get('/productos', (req, res) => {
+        res.send(data)
 })
 
-app.get('/productoRandom', (request, response) => {
-    let randomNumber = getRandomInt(1, 4)
-    response.send(`${seeProducts.getById(randomNumber)}`)
+app.get('/productoRandom', (req, res) => {
+    let min, max
+    let result
+
+    if(data.length > 0) {
+        min = 1
+        max = data.length + 1
+        let randomNumber = getRandomInt(min, max)
+        const getData = async () => {
+            result = await seeProducts.getById(randomNumber)
+            res.send(result)
+        }
+        getData()
+    } else {
+        result = 'No hay productos'
+        res.send(result)
+    }
 })
